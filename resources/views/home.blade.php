c659de130f7fce7a3c557ad67ee0d7db49da64a1
Clarence Bañagaso <131096266+Eboi999@users.noreply.github.com>
2025-02-22 23:17:30 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>Home Page </title>
 
     @vite(['resources/css/app.css'])
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 </head>
 <body class="flex justify-center items-center h-screen bg-gray-100 text-gray-800 text-center">
     <div>
-        <h1 class="text-3xl font-semibold">Welcome to My Home Page</h1>
+        <h1 class="text-3xl font-semibold">Welcome to My Simple Home Page :))</h1>
     </div>
 </body>
 </html>
